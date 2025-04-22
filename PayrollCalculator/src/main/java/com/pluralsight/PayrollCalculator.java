6aba5176fe9677335e93ade91fb146f1abe95201
Viktoriia Breha <viktoriia.breha@gmail.com>
2025-04-22 13:30:58 -0400

    Payroll Calculator

--- PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,33 +1,34 @@
 package com.pluralsight;
+
 import java.io.*;
 import java.util.regex.Pattern;
 
 public class PayrollCalculator {
     public static void main(String[] args) {
-try{
-    FileReader fileReader = new FileReader("src/main/resources/DataFiles/employees.csv");
-    BufferedReader bufReader = new BufferedReader(fileReader);
-    String input;
+        try {
+            FileReader fileReader = new FileReader("src/main/resources/DataFiles/employees.csv");
+            BufferedReader bufReader = new BufferedReader(fileReader);
+            String input;
+            while ((input = bufReader.readLine()) != null) {
+                String[] tokens = input.split(Pattern.quote("|"));
 
-    while ((input = bufReader.readLine())!= null){
-        String[] tokens = input.split(Pattern.quote("|"));
+                if (tokens[0].equalsIgnoreCase("id")) {
+                    continue;
+                }
 
-        if (tokens[0].equalsIgnoreCase("id")){
-            continue;
+                int id = Integer.parseInt(tokens[0]);
+                String name = tokens[1];
+                double hoursWorked = Double.parseDouble(tokens[2]);
+                double payRate = Double.parseDouble(tokens[3]);
+
+                Employee employee = new Employee(id, name, hoursWorked, payRate);
+
+                System.out.printf("%d %s %.2f \n", employee.employeeId(), employee.name(), employee.getGrossPay());
+
+            }
+            bufReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        int id = Integer.parseInt(tokens[0]);
-        String name = tokens[1];
-        double hoursWorked = Double.parseDouble(tokens[2]);
-        double payRate = Double.parseDouble(tokens[3]);
-
-        Employee employee = new Employee( id , name , hoursWorked, payRate);
-
-        System.out.printf("%d %s %.2f \n", employee.employeeId(), employee.name(), employee.getGrossPay());
-
-    } bufReader.close();
-}catch (IOException e) {
-    e.printStackTrace();
-}
     }
 }
